7ca57cc4c41196359db05011aeb53c1354a8c4bb
carinahabitzl <carina.habitzl@edu.campus02.at>
2021-10-08 19:58:50 +0200

    variablen deklariert

--- src/calculator.java
+++ src/calculator.java
@@ -2,6 +2,17 @@ public class calculator {
 
     public static void main(String[] args) {
 
+    int a = 1;
+    double b = 345.9;
+    boolean c = false;
+    char d = 'c';
+    String e = "Hallo";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
 
     }
 }
